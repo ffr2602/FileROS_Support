72d6c769f6cda0627560e3ac61895420bb2e371b
Fahmi Fathur Rohman <100126219+ffr2602@users.noreply.github.com>
2024-01-09 23:51:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ sudo apt install ros-$ros-nav2-bringup -y
 sudo apt install ros-$ros-navigation2 -y
 sudo apt install ros-$ros-slam-toolbox -y
 sudo apt install ros-$ros-robot-localization -y
+sudo apt install ros-$ros-bno055 -y
+sudo apt install ros-$ros-laser-filters -y
 sudo apt install joystick jstest-gtk evtest -y
 sudo apt install python3-smbus -y
 sudo apt install python3-pip -y
